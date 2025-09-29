60632bf39c36e37afa2f960f2daa9320cda93a73
Arielbazar <bazarariel1>
2025-09-29 14:06:40 +0800

    intial commit to this project

---
+++ src/Javaprac.java
@@ -0,0 +1,27 @@
+public class Javaprac {
+    public static void main(String[]args){
+        int age = 22;
+        int year = 2025;
+        int quantity = 5;
+
+       double price = 17.99;
+       double  gpa = 4.5;
+       double temperature = -12.5;
+
+       char grade = 'A';
+       char symbol = '@';
+       char currency = '$';
+
+       boolean isStudent = true;
+       boolean forSale = false;
+       boolean isOnline = true;
+
+       String name = "Ariel";
+       String food = "Adobo";
+       String email = "bazarariel@gmail.com";
+       String car = "Nissan";
+       String color = "Blue";
+
+        System.out.println("hello my name is " + name + " " + "my favorite food is " + food + " " + "my age is " + age + " " + "on this year of " + year + " " + "I will buy a car brand of " + car + color );
+    }
+}
